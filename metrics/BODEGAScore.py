da3f6c78db7491db8fc892a14651acb6e593f716
piotrmp <piotr.m.przybyla@gmail.com>
2023-06-28 17:24:54 +0200

    Added sentence alignment.

--- metrics/BODEGAScore.py
+++ metrics/BODEGAScore.py
@@ -4,18 +4,21 @@ from bert_score import score
 import editdistance
 
 from metrics.ScorePromise import ScorePromise
+from lambo.segmenter.lambo import Lambo
+from OpenAttack.text_process.tokenizer.punct_tokenizer import PunctTokenizer
 
 BATCH_SIZE = 16
-
 
 
 class BODEGAScore(OpenAttack.AttackMetric):
     NAME = "BODEGA Score"
     
-    def __init__(self, device, align_sentences=False):
+    def __init__(self, device, task, align_sentences=False):
         self.promises = []
         self.device = device
+        self.task = task
         self.align_sentences = align_sentences
+        self.tokenizerOA = PunctTokenizer()
     
     def after_attack(self, input, adversarial_sample):
         s1 = input['x']
@@ -24,24 +27,37 @@ class BODEGAScore(OpenAttack.AttackMetric):
         self.promises.append(promise)
         return promise
     
-    @classmethod
-    def normalise_for_lev(cls, string):
-        return string.lower().replace('\n', ' ').replace(' ', '')
+    def normalise_for_lev(self, string):
+        result = string.lower().replace('\n', ' ')
+        result = self.tokenizerOA.do_detokenize([x[0] for x in self.tokenizerOA.do_tokenize(result)])
+        return result
     
-    @classmethod
-    def normalise_for_bert(cls, string):
+    def normalise_for_bert(self, string):
         if string is None:
-            return None
+            return 'NOT_GENERATED'
         else:
-            return string.replace('\n', ' ')
+            result = string.replace('\n', ' ')
+            result = self.tokenizerOA.do_detokenize([x[0] for x in self.tokenizerOA.do_tokenize(result)])
+            return result
     
     def compute(self):
+        alignments = [[(promise.s1, promise.s2)] for promise in self.promises]
+        if self.align_sentences:
+            print("Aligning sentences...")
+            self.lambo = Lambo.get('English')
+            alignments = self.align_sentences_greedy()
         print("Computing BERT score...")
-        _, _, BERT_F1 = score([str(self.normalise_for_bert(promise.s1)) for promise in self.promises],
-                              [str(self.normalise_for_bert(promise.s2)) for promise in self.promises],
-                              model_type="microsoft/deberta-large-mnli",
-                              lang="en", rescale_with_baseline=True, device=self.device, batch_size=BATCH_SIZE)
-        BERT_F1 = BERT_F1.numpy()
+        BERT_sentences = []
+        BERT_guide = []
+        for i, alignment in enumerate(alignments):
+            BERT_guide.append([])
+            for sent1, sent2 in alignment:
+                BERT_guide[-1].append(len(BERT_sentences))
+                BERT_sentences.append((self.normalise_for_bert(sent1), self.normalise_for_bert(sent2)))
+        _, _, BERT_F1_list = score([pair[0] for pair in BERT_sentences], [pair[1] for pair in BERT_sentences],
+                                   model_type="microsoft/deberta-large-mnli",
+                                   lang="en", rescale_with_baseline=True, device=self.device, batch_size=BATCH_SIZE)
+        BERT_F1_list = BERT_F1_list.numpy()
         B_scores = []
         BERT_scores = []
         Lev_scores = []
@@ -58,17 +74,78 @@ class BODEGAScore(OpenAttack.AttackMetric):
                 lev_dist = editdistance.eval(normalised_s1, normalised_s2)
                 lev_score = 1.0 - lev_dist / max(len(normalised_s1), len(normalised_s2))
                 Lev_scores.append(lev_score)
-                if BERT_F1[i] <= 0:
-                    # BERT Score is calibrated to be *usually* between 0 and 1, but not guaranteed
-                    BERT_F1[i] = 0
-                BERT_scores.append(BERT_F1[i])
-                B_scores.append(BERT_F1[i] * lev_score)
+                BERT_score = 0
+                for idx in BERT_guide[i]:
+                    print(BERT_F1_list[idx])
+                    print(BERT_sentences[idx][0])
+                    print(BERT_sentences[idx][1])
+                    if BERT_F1_list[idx] <= 0:
+                        # BERT Score is calibrated to be *usually* between 0 and 1, but not guaranteed
+                        BERT_score += 0
+                    elif BERT_F1_list[idx] >= 1:
+                        BERT_score += 1
+                    else:
+                        BERT_score += BERT_F1_list[idx]
+                BERT_score = BERT_score / len(BERT_guide[i])
+                BERT_scores.append(BERT_score)
+                B_scores.append(BERT_score * lev_score)
                 successes.append(1.0)
-                if BERT_F1[i] * lev_score > 0.9:
-                    print(str(i + 1))
-                    print("OLD: ")
-                    print(promise.s1)
-                    print("NEW: ")
-                    print(promise.s2)
+                # Might be useful to output most succesful attacks
+                # if BERT_score * lev_score > 0.9:
+                #    print(str(i + 1))
+                #    print("OLD: ")
+                #    print(promise.s1)
+                #    print("NEW: ")
+                #    print(promise.s2)
         return numpy.average(numpy.array(successes)), numpy.average(numpy.array(BERT_scores)), numpy.average(
             numpy.array(Lev_scores)), numpy.average(numpy.array(B_scores))
+    
+    def align_sentences_greedy(self):
+        result = []
+        for promise in self.promises:
+            if promise.s2 is None:
+                result.append([])
+                continue
+            alignment = []
+            doc1_sentences = self.segment_sentences(promise.s1)
+            doc2_sentences = self.segment_sentences(promise.s2)
+            doc2_used_sentences = set()
+            for sentence1 in doc1_sentences:
+                best_sentence2 = None
+                best_distance = len(promise.s1) + len(promise.s2) + 10
+                for sentence2 in doc2_sentences:
+                    lev_dist = editdistance.eval(self.normalise_for_lev(sentence1),
+                                                 self.normalise_for_lev(sentence2)) / max(
+                        len(self.normalise_for_lev(sentence1)), len(self.normalise_for_lev(sentence2)))
+                    if lev_dist < best_distance:
+                        best_distance = lev_dist
+                        best_sentence2 = sentence2
+                alignment.append((sentence1, best_sentence2))
+                doc2_used_sentences.add(best_sentence2)
+            # Might be useful to take into account added sentences
+            # for sentence2 in doc2_sentences:
+            #     if sentence2 not in doc2_used_sentences:
+            #         best_sentence1 = None
+            #         best_distance = len(promise.s1) + len(promise.s2) + 10
+            #         for sentence1 in doc1_sentences:
+            #             lev_dist = editdistance.eval(self.normalise_for_lev(sentence1),
+            #                                          self.normalise_for_lev(sentence2))
+            #             if lev_dist < best_distance:
+            #                 best_distance = lev_dist
+            #                 best_sentence1 = sentence1
+            #         alignment.append((best_sentence1, sentence2))
+            result.append(alignment)
+        return result
+    
+    def segment_sentences(self, string):
+        result = []
+        if self.task == 'PR2':
+            result = [string]
+        elif self.task== 'FC':
+            result = string.split('~')
+        elif self.task == 'RD' or self.task =='HN':
+            document = self.lambo.segment(string)
+            for turn in document.turns:
+                for sentence in turn.sentences:
+                    result.append(sentence.text)
+        return result
--- runs/attack.py
+++ runs/attack.py
@@ -25,7 +25,7 @@ np.random.seed(0)
 
 # Running variables
 print("Preparing the environment...")
-task = 'HN'
+task = 'PR2'
 targeted = False
 attack = 'DeepWordBug'
 victim_model = 'BiLSTM'
@@ -90,7 +90,7 @@ else:
     dataset = dataset.remove_columns(["text1", "text2"])
 
 dataset = dataset.remove_columns(["fake"])
-#dataset = dataset.select(range(25))
+dataset = dataset.select(range(10))
 
 # Filter data
 if targeted:
@@ -131,7 +131,7 @@ with no_ssl_verify():
 
 # Run the attack
 print("Evaluating the attack...")
-scorer = BODEGAScore(victim_device)
+scorer = BODEGAScore(victim_device, task, align_sentences=True)
 with no_ssl_verify():
     attack_eval = OpenAttack.AttackEval(attacker, victim, language='english', metrics=[
         scorer  # , OpenAttack.metric.EditDistance()
